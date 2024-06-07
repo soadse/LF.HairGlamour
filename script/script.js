1f437edb09d8f7bd0dd7b0359ec4d12e48ddf8d2
Rodrigo Andrade <soadse.ra@gmail.com>
2024-06-06 23:17:20 -0300

    ..

--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
 var open = document.querySelector('#open')
 var close = document.querySelector('#closee')
+var pag02 = document.querySelector('.pag02')
+var pag03 = document.querySelector('.pag03')
+
 
 function abrir(){
     open.style.display = 'none'
@@ -10,3 +13,9 @@ function fechar(){
     open.style.display = 'flex'
     close.style.display = 'none'
 }
+
+function secoes(){
+    pag02.style.display = 'block'
+    pag03.style.display = 'block'
+}
+setTimeout(secoes, 5000)
